a5bfb416e23e02564c9e2e3856589aa26149d1ba
SoarinSkySagar <sagarrana.pvt@gmail.com>
2024-02-03 11:33:46 +0530

    auth router and services

---
+++ server/routes/auth.router.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const passport = require('passport');
+const path = require('path')
+
+const authRouter = express.Router()
+
+authRouter.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'auth.html'))
+})
+
+authRouter.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'login.html'))
+})
+
+authRouter.get('/google',
+  passport.authenticate('google', {
+    scope: ['email'],
+  })
+);
+
+authRouter.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failure', successRedirect: '/dashboard' }));
+
+authRouter.post('/email',
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/auth/failure',
+    failureFlash: true,
+    successFlash: 'Welcome!',
+  })
+);
+
+authRouter.get('/logout', (req, res) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+    });
+  });
+
+authRouter.get('/failure', (req, res) => {
+    res.send('Failed to authenticate..');
+});
+
+module.exports = authRouter
---
+++ server/services/mongo.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const MONGO_URL = process.env.MONGO_URL
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connected!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err)
+})
+
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL)
+}
+
+async function mongoDisconnect() {
+    await mongoose.disconnect()
+}
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect
+}
